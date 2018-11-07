b9f7fd565700ff4558eca966932d0c267d2186a7
Lars van Vliet <123321@student.drenthecollege.nl>
2018-11-07 14:29:24 +0100

    Titel toegevoegd en opgemaakt met css

--- home.php
+++ home.php
@@ -1,6 +1,13 @@
+</div>
 <?php
 include 'header.php';
 ?>
+
+<div class="title">
+  <h3>Topic homepagina Mijn Forum</h3>
+</div>
+<div id="wrapper">
+
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
   in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
